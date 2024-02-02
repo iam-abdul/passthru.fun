bfa529bb4871c50053664d64695c56a0b2016f2e
abd038 <mohammadabdulrehman739@gmail.com>
2024-02-02 18:02:08 +0530

    refactored and added a method to request domains

--- main.go
+++ main.go
@@ -1,149 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"flag"
-	"fmt"
-	"math/rand"
-	"net"
-	"net/http"
-	"strings"
-	"time"
 
-	"github.com/iam-abdul/go-tcp-tunnel/client"
+	"github.com/iam-abdul/go-tcp-tunnel/server"
 )
-
-func GenerateRandomSubDomain() string {
-	domain := "abd.com"
-
-	rand.NewSource(time.Now().UnixNano())
-
-	subdomain := fmt.Sprintf("%x", rand.Int63())
-
-	return subdomain + "." + domain
-}
-
-// i need to rewrite the server side code like client side
-// when a client connects, i should identify if it is tcp connection
-// or http connection
-// if it is tcp connection, i should store the connection object
-// in a map with the randomSubDomain as the key
-
-// if it is http connection, i should read the host header
-// and get the connection object from the map
-// and write the request to the connection object
-// and read the response from the connection object
-// and write the response to the http connection object
-
-func ServerSide() {
-
-	// start a tcp server on port 8888
-	// listen for incoming connections
-
-	sln, err := net.Listen("tcp", "192.168.80.45:8888")
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer sln.Close()
-	// a map to store the connection objects
-	// with the randomSubDomain as the key
-
-	connections := make(map[string]net.Conn)
-
-	// accept incoming connections concurrently
-
-	for {
-		conn, err := sln.Accept()
-
-		if err != nil {
-			panic(err)
-		}
-
-		go func(c net.Conn) {
-			// defer conn.Close()
-
-			// on first connection, send a welcome message
-			// to the client
-
-			randomSubDomain := GenerateRandomSubDomain()
-			// store the connection object in a map
-			// with the randomSubDomain as the key
-			connections[randomSubDomain] = c
-
-			// read if there is any header with host field
-
-			c.Write([]byte(randomSubDomain + "\n"))
-
-			var buf []byte
-
-			for {
-				n, err := c.Read(buf)
-
-				if err != nil {
-					if err.Error() == "EOF" {
-						fmt.Println("client disconnected")
-						break
-					} else {
-						panic(err)
-					}
-				}
-
-				requestString := string(buf[:n])
-				reader := strings.NewReader(requestString)
-
-				// parsing the string to a http request object
-				req, err := http.ReadRequest(bufio.NewReader(reader))
-
-				if err != nil {
-					panic(err)
-				}
-
-				// get the host field from the header
-				host := req.Host
-
-				// get the connection object from the map
-				// using the host field as the key
-				conn, ok := connections[host]
-
-				if !ok {
-					fmt.Println("connection not found")
-					break
-				}
-
-				// write the request to the connection object
-				// which is the client connection
-
-				// convert request string to byte array
-				// and write to the connection object
-
-				_, err = conn.Write([]byte(requestString))
-
-				if err != nil {
-					panic(err)
-				}
-
-			}
-
-		}(conn)
-	}
-
-}
 
 func main() {
 
-	usedAs := flag.String("usedAs", "", "used as server or client")
+	typeOf := flag.String("type", "server", "type of the program to run")
 
-	flag.Parse()
-
-	fmt.Println("used as --> ", *usedAs)
-
-	if *usedAs == "server" {
-		fmt.Println("used as server")
-		ServerSide()
-	} else {
-		client.RunAsClient("3000")
+	if *typeOf == "server" {
+		server.StartNewServer("localhost:8888")
 	}
 
 }
---
+++ server/server.go
@@ -0,0 +1,112 @@
+package server
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+type Server struct {
+	ListenAddress string
+	Ln            net.Listener
+	lockConn      sync.Mutex
+	connections   map[string]*net.Conn
+}
+
+func (s *Server) startServer() {
+	ln, err := net.Listen("tcp", s.ListenAddress)
+
+	if err != nil {
+		panic(err)
+	}
+
+	s.Ln = ln
+
+	s.acceptLoop()
+}
+
+func (s *Server) acceptLoop() {
+	for {
+		conn, err := s.Ln.Accept()
+
+		if err != nil {
+			panic(err)
+		}
+
+		go s.readLoop(conn)
+	}
+}
+
+func (s *Server) readLoop(conn net.Conn) {
+	buf := make([]byte, 2048)
+
+	for {
+		n, err := conn.Read(buf)
+
+		if err != nil {
+			if err.Error() == "EOF" {
+				fmt.Println("client disconnected")
+				break
+			} else {
+				panic(err)
+			}
+		}
+
+		reader := bufio.NewReader(bytes.NewReader(buf[:n]))
+
+		req, err := http.ReadRequest(reader)
+
+		if err != nil {
+			fmt.Println("error parsing request string to http request object ", err)
+		} else if req.Method == "POST" {
+			fmt.Println("received", req.Method, req.Host, req.Body)
+
+			bodyBytes, err := io.ReadAll(req.Body)
+
+			if err != nil {
+				fmt.Println("error reading request body ", err)
+			}
+
+			// if the request is to the domain abdul.com then we will create a new hostname
+			if req.Host == "abdul.com" {
+				s.createHostName(string(bodyBytes), &conn, req)
+			}
+		}
+
+		// fmt.Println("received", string(buf[:n]))
+	}
+}
+
+func (s *Server) createHostName(requestedName string, conn *net.Conn, req *http.Request) {
+	// check if the requested name is already in use
+	// if it is then return an error
+
+	// if it is not then create a new hostname and return the hostname
+	s.lockConn.Lock()
+	if s.connections[requestedName] == nil {
+		s.connections[requestedName] = conn
+
+		// send the response back to the client
+		resp := "HTTP/1.1 200 OK\r\nContent-Length: " + strconv.Itoa(len(requestedName)) + "\r\n\r\n" + requestedName
+		(*conn).Write([]byte(resp))
+	} else {
+		// send the response back to the client
+		resp := "HTTP/1.1 400 Bad Request\r\nContent-Length: " + strconv.Itoa(len("Hostname already in use")) + "\r\n\r\n" + "Hostname already in use"
+		(*conn).Write([]byte(resp))
+	}
+	s.lockConn.Unlock()
+
+}
+
+func StartNewServer(listenAddress string) {
+	server := Server{
+		ListenAddress: listenAddress,
+		connections:   make(map[string]*net.Conn),
+	}
+	server.startServer()
+}
