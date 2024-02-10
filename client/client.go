90a152bbe250d1c62ba7e50a3914208dada60da6
abd038 <mohammadabdulrehman739@gmail.com>
2024-02-11 00:02:25 +0530

    working

--- client/client.go
+++ client/client.go
@@ -1,81 +1,30 @@
 package client
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"net"
 )
 
-func joinConnections(conn1 net.Conn, conn2 net.Conn) {
-	// Copy data from conn1 to conn2
-	go func() {
-		fmt.Println("copying data from client to server")
-		_, err := io.Copy(conn1, conn2)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	// Copy data from conn2 to conn1
-	go func() {
-		fmt.Println("copying data from server to client")
-		_, err := io.Copy(conn2, conn1)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-}
-
 func RunAsClient() {
-	// start tcp connection to the server
-
-	serverConnection, err := net.Dial("tcp", "localhost:8888")
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:8888")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	// requesting the domain
-	serverConnection.Write([]byte("domain abdul.com"))
-
-	// reading the response
-	isDomainAvailable := make([]byte, 2048)
-	n, err := serverConnection.Read(isDomainAvailable)
-
+	conn, err := net.DialTCP("tcp", nil, addr)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	fmt.Println("domain availability ", string(isDomainAvailable[:n]))
+	defer conn.Close()
 
-	if string(isDomainAvailable[:n]) == "true" {
-
-		// client tcp connection
-		_, err := net.Dial("tcp", "localhost:3000")
+	buf := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buf)
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
+		log.Println(string(buf[:n]))
 
-		// join the connections
-
-		// go joinConnections(serverConnection, clientConnection)
-
-		for {
-			// read the response from the server
-			buf := make([]byte, 2048)
-			n, err := serverConnection.Read(buf)
-			fmt.Println("response from the server ", string(buf[:n]))
-			if err != nil {
-				if err == io.EOF {
-					fmt.Println("Server connection closed")
-					break
-				}
-				panic(err)
-			}
-		}
-
-	} else {
-		fmt.Println("domain is not available")
 	}
-
 }
--- server/server.go
+++ server/server.go
@@ -1,141 +1,79 @@
 package server
 
 import (
-	"bufio"
+	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net"
-	"net/http"
-	"strings"
 )
 
 type Server struct {
-	Addr        string
-	Connections map[string]net.Conn
+	connections map[string]*net.TCPConn
 }
 
-func (s *Server) handleConnection(conn net.Conn) {
-	// we will look at the first few words of data
-	// to determine if it is a request or a tcp connection
-
+func (s *Server) handleConnection(conn *net.TCPConn) {
+	defer conn.Close()
+	buf := make([]byte, 1024)
 	for {
-		buf := make([]byte, 2048)
 		n, err := conn.Read(buf)
 		if err != nil {
-			if err.Error() == "EOF" {
-				// clear up the connection
-				fmt.Println("connection closed")
-				disconnectedDomain := ""
-				for domain, connection := range s.Connections {
-					if connection == conn {
-						disconnectedDomain = domain
-						break
+			if errors.Is(err, io.EOF) {
+				fmt.Println("Client disconnected ", err)
+				delete(s.connections, conn.RemoteAddr().String())
+				break
+			}
+		}
+		fmt.Println("Received from client: ", string(buf[:n]))
+		fmt.Println("Number of connections: ", len(s.connections))
+
+		if len(s.connections) > 1 {
+			// we will forward the message to the other client
+			for _, c := range s.connections {
+				if c != conn {
+					_, err := c.Write(buf[:n])
+					// lets use copy instead of Write
+					// _, err := io.Copy(c, conn)
+					if err != nil {
+						fmt.Println("Error writing to client: ", err)
 					}
 				}
-
-				if disconnectedDomain != "" {
-					fmt.Println("domain disconnected ", disconnectedDomain)
-					delete(s.Connections, disconnectedDomain)
-				}
-
-				conn.Close()
-				break
-			} else {
-				panic(err)
 			}
-		}
-
-		// fmt.Println("data received ", string(buf[:n]))
-
-		words := strings.Fields(string(buf[:n]))
-
-		if words[0] == "domain" {
-			fmt.Println("domain request received ", words[1])
-
-			// check if the domain is available
-			if s.Connections[words[1]] != nil {
-				conn.Write([]byte("false"))
-			} else {
-				// store the connection
-				s.Connections[words[1]] = conn
-				conn.Write([]byte("true"))
-			}
-
-		} else {
-			// this is a http request from the some server
-			// find the host and forward the request to the host
-			// fmt.Println("http request received ", string(buf[:n]))
-			req, err := http.ReadRequest(bufio.NewReader(strings.NewReader(string(buf[:n]))))
-
-			if err != nil {
-				if err.Error() == "EOF" {
-					fmt.Println("EOF error")
-				} else {
-					panic(err)
-				}
-			}
-
-			host := req.Host
-			fmt.Println("outside server to client ", host)
-			// fmt.Println("host ", host)
-
-			targetConn := s.Connections[host]
-			fmt.Println("target connection ", targetConn)
-
-			if targetConn == nil {
-				fmt.Println("target host not found")
-				continue
-			}
-
-			// join the target connection with the client connection
-			go func() {
-
-				fmt.Println("copying data from server to client")
-				copied, err := io.Copy(targetConn, conn)
-				fmt.Println("copied data ", copied)
-				if err != nil {
-					fmt.Println("error in copying data ", err)
-				}
-			}()
-
-			// go func() {
-			// 	fmt.Println("copying data from client to server")
-			// 	_, err := io.Copy(conn, targetConn)
-			// 	if err != nil {
-			// 		fmt.Println("error in copying data ", err)
-			// 	}
-			// }()
-
-			if err != nil {
-				fmt.Println("error in copying data ", err)
-			}
-
 		}
 	}
-
 }
 
-func StartNewServer(addr string) {
-
-	server := &Server{
-		Addr:        addr,
-		Connections: make(map[string]net.Conn),
+func (s *Server) start() {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:8888")
+	if err != nil {
+		// handle error
+		log.Fatal("Error resolving TCP address: ", err)
 	}
 
-	// start a tcp server
-	hostConn, err := net.Listen("tcp", server.Addr)
+	ln, err := net.ListenTCP("tcp", addr)
 	if err != nil {
-		panic(err)
+		// handle error
+		log.Fatal("Error listening on TCP: ", err)
 	}
 
 	for {
-		conn, err := hostConn.Accept()
+		conn, err := ln.AcceptTCP()
 		if err != nil {
-			panic(err)
+			if errors.Is(err, io.EOF) {
+				fmt.Println("Err accepting connection from client: ", err)
+				continue
+			}
 		}
+		s.connections[conn.RemoteAddr().String()] = conn
+		fmt.Println("New connection from: ", conn.RemoteAddr().String())
 
-		go server.handleConnection(conn)
-
+		go s.handleConnection(conn)
 	}
+}
 
+func StartNewServer(address string) {
+	server := &Server{
+		connections: make(map[string]*net.TCPConn),
+	}
+	server.start()
 }
